0eeebf09715ae951bb5d669f79a7430b6887571a
longpv <long05062000@gmail.com>
2024-12-17 09:30:58 +0700

    coding

--- wp-content/themes/ulytan/page-dich_thuat_cong_chung.php
+++ wp-content/themes/ulytan/page-dich_thuat_cong_chung.php
@@ -76,19 +76,15 @@ if ($choice_reason):
 <?php endif; ?>
 
 <?php
-$achieve_brand_title = get_field('achieve_brand_title') ?? '';
+$achieve_brand_title = get_field('achieve_brand_title') ?: '“Doanh nghiệp đạt thương hiệu nổi tiếng Việt Nam với 15 năm kinh nghiệm”';
 $achieve_brand_image = get_field('achieve_brand_image') ?? [];
 if ($achieve_brand_image) :
 ?>
-	<section class="achieve_brand secSpace" id="scroll_1">
+	<section class="achieve_brand secSpace--bottom" id="scroll_1">
 		<div class="container">
 			<h2 class="home_news_title text-center mb-4">
 				<?php
-				if ($achieve_brand_title) {
-					echo $achieve_brand_title;
-				} else {
-					echo '“Doanh nghiệp đạt thương hiệu nổi tiếng Việt Nam với 15 năm kinh nghiệm”';
-				}
+				echo $achieve_brand_title;
 				?>
 			</h2>
 			<div class="row justify-content-center">
@@ -117,7 +113,7 @@ $customer_object = get_field('customer_object') ?? [];
 $customer_object_title = get_field('customer_object_title') ?: 'ĐỐI TƯỢNG KHÁCH HÀNG CỦA ULYTAN';
 if ($customer_object):
 ?>
-	<section class="choice_reason secSpace">
+	<section class="choice_reason secSpace--bottom">
 		<div class="container">
 			<h2 class="home_news_title text-center mb-4">
 				<?php echo $customer_object_title; ?>
@@ -136,6 +132,91 @@ if ($customer_object):
 							</h3>
 							<div class="choice_reason_item_desc">
 								<?php echo $item['description']; ?>
+							</div>
+						</div>
+					</div>
+				<?php endforeach; ?>
+			</div>
+		</div>
+	</section>
+<?php endif; ?>
+
+
+<?php
+$video_customer_title = get_field('video_customer_title') ?: 'KHÁCH HÀNG NÓI VỀ ULYTAN';
+$video_customer = get_field('video_customer') ?? [];
+if ($video_customer) {
+	$args = array(
+		'post_type' => 'video_customer',
+		'posts_per_page' => -1,
+		'post__in' => $video_customer,
+		'orderby' => 'post__in',
+	);
+} else {
+	$args = array(
+		'post_type' => 'video_customer',
+		'posts_per_page' => 6,
+	);
+}
+
+$query = new WP_Query($args);
+if ($query->have_posts()):
+?>
+	<section class="video_customer secSpace bg-light" id="scroll_4">
+		<div class="container">
+			<h2 class="sec_title text-center">
+				<?php echo $video_customer_title; ?>
+			</h2>
+			<div class="video_customer_slider">
+				<?php
+				while ($query->have_posts()):
+					$query->the_post();
+				?>
+					<div>
+						<div class="video_customer_item">
+							<?php
+							$featured_image = get_field('featured_image');
+							$iframe_video = get_field('iframe_video');
+							video_popup($iframe_video, $featured_image);
+							?>
+						</div>
+					</div>
+				<?php
+				endwhile;
+				?>
+			</div>
+		</div>
+	</section>
+<?php
+endif;
+wp_reset_postdata();
+?>
+
+
+<?php
+$use_service = get_field('use_service') ?? [];
+$use_service_title = get_field('use_service_title') ?: 'DỊCH VỤ CỦA ULYTAN SỬ DỤNG KHI NÀO?';
+if ($use_service):
+?>
+	<section class="choice_reason secSpace">
+		<div class="container">
+			<h2 class="home_news_title text-center mb-4">
+				<?php echo $use_service_title; ?>
+			</h2>
+			<div class="row row_32 justify-content-center">
+				<?php foreach ($use_service as $key => $item) : ?>
+					<div class="col-md-6 col-lg-4">
+						<div class="choice_reason_item">
+							<?php if ($item['icon']): ?>
+								<div class="choice_reason_item_img">
+									<img src="<?php echo $item['icon']; ?>" alt="<?php echo $item['title']; ?>">
+								</div>
+							<?php endif; ?>
+							<h3 class="choice_reason_item_title" data-mh="choice_reason_item_title">
+								<?php echo $item['title']; ?>
+							</h3>
+							<div class="editor choice_reason_item_content">
+								<?php echo $item['content']; ?>
 							</div>
 						</div>
 					</div>
@@ -198,38 +279,42 @@ if ($price_table) :
 	</section>
 <?php endif; ?>
 
-<section class="advisory secSpace bg-primary" id="scroll_6">
-	<div class="container">
-		<div class="row justify-content-center">
-			<div class="col-lg-9">
-				<div class="row">
-					<div class="col-md-6 mb-4 mb-md-0">
-						<?php
-						$form_advisory = get_field('form_advisory') ?? '';
-						if ($form_advisory):
-						?>
+<?php
+$form_advisory_title = get_field('form_advisory_title') ?: 'Hãy để lại thông tin để được tư vấn miễn phí!';
+$form_advisory = get_field('form_advisory') ?? '';
+if ($form_advisory):
+?>
+	<section class="advisory secSpace bg-primary" id="scroll_6">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-lg-9">
+					<div class="row">
+						<div class="col-md-6 mb-4 mb-md-0">
+
 							<div class="form_advisory">
 								<div class="form_advisory_title">
-									Hãy để lại thông tin để được tư vấn miễn phí!
+									<?php echo $form_advisory_title; ?>
 								</div>
 								<?php echo do_shortcode('[contact-form-7 id="' . $form_advisory . '"]'); ?>
 							</div>
-						<?php
-						endif;
-						?>
-					</div>
-					<div class="col-md-6">
-						<div class="image_advisory_wrap">
-							<img class="image_advisory" src="<?php echo get_template_directory_uri() . '/assets/images/image_advisory.png'; ?>" alt="image advisory">
+
+						</div>
+						<div class="col-md-6">
+							<div class="image_advisory_wrap">
+								<img class="image_advisory" src="<?php echo get_template_directory_uri() . '/assets/images/image_advisory.png'; ?>" alt="image advisory">
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-</section>
+	</section>
+<?php
+endif;
+?>
 
 <?php
+$services_title = get_field('services_title') ?: 'CÁC DỊCH VỤ BỔ SUNG';
 $services = get_field('services') ?? [];
 if ($services) {
 	$args = array(
@@ -251,7 +336,7 @@ if ($query->have_posts()):
 	<section class="service secSpace" id="scroll_3">
 		<div class="container">
 			<h2 class="sec_title text-center">
-				CÁC DỊCH VỤ BỔ SUNG
+				<?php echo $services_title; ?>
 			</h2>
 
 			<div class="service_list">
@@ -267,55 +352,6 @@ if ($query->have_posts()):
 					endwhile;
 					?>
 				</div>
-			</div>
-		</div>
-	</section>
-<?php
-endif;
-wp_reset_postdata();
-?>
-
-<?php
-$video_customer = get_field('video_customer') ?? [];
-if ($video_customer) {
-	$args = array(
-		'post_type' => 'video_customer',
-		'posts_per_page' => -1,
-		'post__in' => $video_customer,
-		'orderby' => 'post__in',
-	);
-} else {
-	$args = array(
-		'post_type' => 'video_customer',
-		'posts_per_page' => 6,
-	);
-}
-
-$query = new WP_Query($args);
-if ($query->have_posts()):
-?>
-	<section class="video_customer secSpace bg-light" id="scroll_4">
-		<div class="container">
-			<h2 class="sec_title text-center">
-				KHÁCH HÀNG NÓI VỀ ULYTAN
-			</h2>
-			<div class="video_customer_slider">
-				<?php
-				while ($query->have_posts()):
-					$query->the_post();
-				?>
-					<div>
-						<div class="video_customer_item">
-							<?php
-							$featured_image = get_field('featured_image');
-							$iframe_video = get_field('iframe_video');
-							video_popup($iframe_video, $featured_image);
-							?>
-						</div>
-					</div>
-				<?php
-				endwhile;
-				?>
 			</div>
 		</div>
 	</section>
@@ -414,6 +450,7 @@ wp_reset_postdata();
 ?>
 
 <?php
+$related_posts_title = get_field('related_posts_title') ?: 'Bài viết liên quan';
 $related_posts = get_field('related_posts') ?? [];
 if ($related_posts) {
 	$args = array(
@@ -435,7 +472,7 @@ if ($query->have_posts()):
 	<section class="secSpace--bottom related_posts">
 		<div class="container">
 			<h2 class="home_news_title mb-4">
-				Bài viết liên quan
+				<?php echo $related_posts_title; ?>
 			</h2>
 			<div class="row row_24">
 				<?php
