ecc694e884ab18071d1bfed6532627ff58ba7872
longpv <longpv@codluck.com>
2024-12-20 23:47:36 +0700

    sửa màu popup

--- wp-content/themes/ulytan/page-ctv.php
+++ wp-content/themes/ulytan/page-ctv.php
@@ -785,7 +785,7 @@ get_header();
 						</div>
 						<div class="list_link">
 							<?php if ($tiktok): ?>
-								<a href="<?php echo $tiktok; ?>" target="_blank">
+								<a href="<?php echo $tiktok; ?>" target="_blank" class="mxh_tiktok">
 									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
 										<path d="M448 209.9a210.1 210.1 0 0 1 -122.8-39.3V349.4A162.6 162.6 0 1 1 185 188.3V278.2a74.6 74.6 0 1 0 52.2 71.2V0l88 0a121.2 121.2 0 0 0 1.9 22.2h0A122.2 122.2 0 0 0 381 102.4a121.4 121.4 0 0 0 67 20.1z" />
 									</svg>
@@ -793,7 +793,7 @@ get_header();
 							<?php endif; ?>
 
 							<?php if ($facebook): ?>
-								<a href="<?php echo $facebook; ?>" target="_blank">
+								<a href="<?php echo $facebook; ?>" target="_blank" class="mxh_fb">
 									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
 										<path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
 									</svg>
@@ -801,7 +801,7 @@ get_header();
 							<?php endif; ?>
 
 							<?php if ($youtube): ?>
-								<a href="<?php echo $youtube; ?>" target="_blank">
+								<a href="<?php echo $youtube; ?>" target="_blank" class="mxh_yt">
 									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
 										<path d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z" />
 									</svg>
@@ -818,13 +818,13 @@ get_header();
 					$share_link = get_permalink();
 					?>
 					<div class="social_share_post">
-						<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $share_link; ?>" onclick="window.open(this.href, this.target, 'width=500,height=500'); return false;" class="social_share_post_facebook">
+						<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $share_link; ?>" onclick="window.open(this.href, this.target, 'width=500,height=500'); return false;" class="social_share_post_facebook mxh_fb">
 							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
 								<path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
 							</svg>
 						</a>
 
-						<a href="https://twitter.com/home?status=<?php echo $share_link; ?>" onclick="window.open(this.href, this.target, 'width=500,height=500'); return false;" class="social_share_post_twitter">
+						<a href="https://twitter.com/home?status=<?php echo $share_link; ?>" onclick="window.open(this.href, this.target, 'width=500,height=500'); return false;" class="social_share_post_twitter mxh_tw">
 							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
 								<path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
 							</svg>
--- wp-content/themes/ulytan/single.php
+++ wp-content/themes/ulytan/single.php
@@ -99,42 +99,7 @@ get_header();
 					<?php the_content(); ?>
 				</div>
 			</div>
-			<div class="col-lg-3">
-				<div class="post_sidebar_right">
-					<div class="share_post mb-4">
-						<div class="share_post_title">
-							Chia sẻ bài viết
-						</div>
-						<div class="share_post_mxh">
-							<a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" onclick="window.open(this.href, this.target, 'width=500,height=500'); return false;" class="share_post_mxh_item">
-								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-									<path d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z" />
-								</svg>
-							</a>
-							<a href="javascript:void(0);" onclick="copyToClipboard('#copy2')" class="share_post_mxh_item">
-								<span id="copy2" style="display:none"><?php the_permalink(); ?></span>
-								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-									<path d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z" />
-								</svg>
-							</a>
-						</div>
-					</div>
-
-					<div class="receive_doc">
-						<?php
-						$sign_up_for_exclusive_material = get_field('sign_up_for_exclusive_material', 'option') ?? null;
-						if ($sign_up_for_exclusive_material):
-						?>
-							<div class="share_post_title">
-								Đăng ký nhận tài liệu độc quyền
-							</div>
-							<div class="contact_form_7">
-								<?php echo do_shortcode('[contact-form-7 id="' . $sign_up_for_exclusive_material . '"]'); ?>
-							</div>
-						<?php endif; ?>
-					</div>
-				</div>
-
+			<div class="col-lg-3 right_sticky">
 				<div class="sidebar_lien_he">
 					<form id="page_contact_form" class="page_contact_form" enctype="multipart/form-data">
 						<div class="page_contact_title">
@@ -545,7 +510,7 @@ endif;
 wp_reset_postdata();
 ?>
 
-<section class="bg-light secSpace--bottom">
+<section class="bg-light-1 secSpace--bottom-1 single_post_mxh">
 	<div class="container">
 		<?php
 		$post_id = get_the_ID();
