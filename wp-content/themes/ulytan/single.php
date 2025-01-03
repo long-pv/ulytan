cc21908ce9565f7be20330b9116e182c27dc0345
longpv <longpv@codluck.com>
2025-01-03 23:43:38 +0700

    chuyển phần like share ở ngay dưới nội dung bài viết

--- wp-content/themes/ulytan/single.php
+++ wp-content/themes/ulytan/single.php
@@ -105,6 +105,52 @@ get_header();
 					</div>
 					<?php the_content(); ?>
 				</div>
+
+				<section class="bg-light-1 secSpace--bottom-1 single_post_mxh_1">
+					<div class="container_1">
+						<?php
+						$post_id = get_the_ID();
+						$session_key = "reaction_$post_id";
+						$current_reaction = isset($_COOKIE[$session_key]) ? $_COOKIE[$session_key] : '';
+						?>
+						<div class="reaction_buttons">
+							<button class="reaction_button_item like_button <?php echo $current_reaction === 'like' ? 'active' : ''; ?>" data-post-id="<?php echo $post_id; ?>">
+								<span class="reaction_buttons_icon">
+									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+										<path d="M313.4 32.9c26 5.2 42.9 30.5 37.7 56.5l-2.3 11.4c-5.3 26.7-15.1 52.1-28.8 75.2l144 0c26.5 0 48 21.5 48 48c0 18.5-10.5 34.6-25.9 42.6C497 275.4 504 288.9 504 304c0 23.4-16.8 42.9-38.9 47.1c4.4 7.3 6.9 15.8 6.9 24.9c0 21.3-13.9 39.4-33.1 45.6c.7 3.3 1.1 6.8 1.1 10.4c0 26.5-21.5 48-48 48l-97.5 0c-19 0-37.5-5.6-53.3-16.1l-38.5-25.7C176 420.4 160 390.4 160 358.3l0-38.3 0-48 0-24.9c0-29.2 13.3-56.7 36-75l7.4-5.9c26.5-21.2 44.6-51 51.2-84.2l2.3-11.4c5.2-26 30.5-42.9 56.5-37.7zM32 192l64 0c17.7 0 32 14.3 32 32l0 224c0 17.7-14.3 32-32 32l-64 0c-17.7 0-32-14.3-32-32L0 224c0-17.7 14.3-32 32-32z" />
+									</svg>
+								</span>
+								(<span class="like_count">
+									<?php echo get_post_meta($post_id, 'likes', true) ?: 0; ?>
+								</span>)
+							</button>
+
+							<a href="javascript:void(0);" onclick="copyToClipboard('#copy2')" class="reaction_button_item">
+								<span id="copy2" style="display:none"><?php the_permalink(); ?></span>
+								Copylink
+							</a>
+
+							<a class="reaction_button_item" href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" onclick="window.open(this.href, this.target, 'width=500,height=500'); return false;" class="share_post_mxh_item">
+								Chia sẻ lên Facebook
+							</a>
+
+							<a class="reaction_button_item" href="https://twitter.com/home?status=<?php echo the_permalink(); ?>" onclick="window.open(this.href, this.target, 'width=500,height=500'); return false;" class="share_post_mxh_item">
+								Chia sẻ lên X (Twitter)
+							</a>
+
+							<button class="reaction_button_item dislike_button <?php echo $current_reaction === 'dislike' ? 'active' : ''; ?>" data-post-id="<?php echo $post_id; ?>">
+								<span class="reaction_buttons_icon">
+									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+										<path d="M313.4 479.1c26-5.2 42.9-30.5 37.7-56.5l-2.3-11.4c-5.3-26.7-15.1-52.1-28.8-75.2l144 0c26.5 0 48-21.5 48-48c0-18.5-10.5-34.6-25.9-42.6C497 236.6 504 223.1 504 208c0-23.4-16.8-42.9-38.9-47.1c4.4-7.3 6.9-15.8 6.9-24.9c0-21.3-13.9-39.4-33.1-45.6c.7-3.3 1.1-6.8 1.1-10.4c0-26.5-21.5-48-48-48l-97.5 0c-19 0-37.5 5.6-53.3 16.1L202.7 73.8C176 91.6 160 121.6 160 153.7l0 38.3 0 48 0 24.9c0 29.2 13.3 56.7 36 75l7.4 5.9c26.5 21.2 44.6 51 51.2 84.2l2.3 11.4c5.2 26 30.5 42.9 56.5 37.7zM32 384l64 0c17.7 0 32-14.3 32-32l0-224c0-17.7-14.3-32-32-32L32 96C14.3 96 0 110.3 0 128L0 352c0 17.7 14.3 32 32 32z" />
+									</svg>
+								</span>
+								(<span class="dislike_count">
+									<?php echo get_post_meta($post_id, 'dislikes', true) ?: 0; ?>
+								</span>)
+							</button>
+						</div>
+					</div>
+				</section>
 			</div>
 			<div class="col-lg-3">
 				<div class="sidebar_lien_he">
@@ -479,52 +525,6 @@ if ($query->have_posts()):
 endif;
 wp_reset_postdata();
 ?>
-
-<section class="bg-light-1 secSpace--bottom-1 single_post_mxh">
-	<div class="container">
-		<?php
-		$post_id = get_the_ID();
-		$session_key = "reaction_$post_id";
-		$current_reaction = isset($_COOKIE[$session_key]) ? $_COOKIE[$session_key] : '';
-		?>
-		<div class="reaction_buttons">
-			<button class="reaction_button_item like_button <?php echo $current_reaction === 'like' ? 'active' : ''; ?>" data-post-id="<?php echo $post_id; ?>">
-				<span class="reaction_buttons_icon">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-						<path d="M313.4 32.9c26 5.2 42.9 30.5 37.7 56.5l-2.3 11.4c-5.3 26.7-15.1 52.1-28.8 75.2l144 0c26.5 0 48 21.5 48 48c0 18.5-10.5 34.6-25.9 42.6C497 275.4 504 288.9 504 304c0 23.4-16.8 42.9-38.9 47.1c4.4 7.3 6.9 15.8 6.9 24.9c0 21.3-13.9 39.4-33.1 45.6c.7 3.3 1.1 6.8 1.1 10.4c0 26.5-21.5 48-48 48l-97.5 0c-19 0-37.5-5.6-53.3-16.1l-38.5-25.7C176 420.4 160 390.4 160 358.3l0-38.3 0-48 0-24.9c0-29.2 13.3-56.7 36-75l7.4-5.9c26.5-21.2 44.6-51 51.2-84.2l2.3-11.4c5.2-26 30.5-42.9 56.5-37.7zM32 192l64 0c17.7 0 32 14.3 32 32l0 224c0 17.7-14.3 32-32 32l-64 0c-17.7 0-32-14.3-32-32L0 224c0-17.7 14.3-32 32-32z" />
-					</svg>
-				</span>
-				(<span class="like_count">
-					<?php echo get_post_meta($post_id, 'likes', true) ?: 0; ?>
-				</span>)
-			</button>
-
-			<a href="javascript:void(0);" onclick="copyToClipboard('#copy2')" class="reaction_button_item">
-				<span id="copy2" style="display:none"><?php the_permalink(); ?></span>
-				Copylink
-			</a>
-
-			<a class="reaction_button_item" href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" onclick="window.open(this.href, this.target, 'width=500,height=500'); return false;" class="share_post_mxh_item">
-				Chia sẻ lên Facebook
-			</a>
-
-			<a class="reaction_button_item" href="https://twitter.com/home?status=<?php echo the_permalink(); ?>" onclick="window.open(this.href, this.target, 'width=500,height=500'); return false;" class="share_post_mxh_item">
-				Chia sẻ lên X (Twitter)
-			</a>
-
-			<button class="reaction_button_item dislike_button <?php echo $current_reaction === 'dislike' ? 'active' : ''; ?>" data-post-id="<?php echo $post_id; ?>">
-				<span class="reaction_buttons_icon">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-						<path d="M313.4 479.1c26-5.2 42.9-30.5 37.7-56.5l-2.3-11.4c-5.3-26.7-15.1-52.1-28.8-75.2l144 0c26.5 0 48-21.5 48-48c0-18.5-10.5-34.6-25.9-42.6C497 236.6 504 223.1 504 208c0-23.4-16.8-42.9-38.9-47.1c4.4-7.3 6.9-15.8 6.9-24.9c0-21.3-13.9-39.4-33.1-45.6c.7-3.3 1.1-6.8 1.1-10.4c0-26.5-21.5-48-48-48l-97.5 0c-19 0-37.5 5.6-53.3 16.1L202.7 73.8C176 91.6 160 121.6 160 153.7l0 38.3 0 48 0 24.9c0 29.2 13.3 56.7 36 75l7.4 5.9c26.5 21.2 44.6 51 51.2 84.2l2.3 11.4c5.2 26 30.5 42.9 56.5 37.7zM32 384l64 0c17.7 0 32-14.3 32-32l0-224c0-17.7-14.3-32-32-32L32 96C14.3 96 0 110.3 0 128L0 352c0 17.7 14.3 32 32 32z" />
-					</svg>
-				</span>
-				(<span class="dislike_count">
-					<?php echo get_post_meta($post_id, 'dislikes', true) ?: 0; ?>
-				</span>)
-			</button>
-		</div>
-	</div>
-</section>
 
 <?php
 $args = array(
